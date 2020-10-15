c92eb075977bb6a8b648f408c4b627b885ab7987
tongyuecheng <1530155265@qq.com>
2020-10-15 15:02:42 +0800

    提交新项目

---
+++ FirstSwift/Controller/Main/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  FirstSwift
+//
+//  Created by tongyuecheng on 2020/10/15.
+//  Copyright © 2020 tongyuecheng. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.isTranslucent = false;
+        
+        selectedIndex = 1
+        
+    }
+    
+    func setupLayout() {
+        // 社区
+        
+    }
+    
+
+    
+
+}
---
+++ FirstSwift/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FirstSwift
+//
+//  Created by tongyuecheng on 2020/9/6.
+//  Copyright © 2020 tongyuecheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
